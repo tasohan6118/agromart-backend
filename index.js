4af9242902d64c6c8fbb517fc4c4653cc52a1b64
sohan611 <tasnimahammed18@cse.pstu.ac.bd>
2026-07-20 20:07:54 +0600

    remove slash

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port= process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'https://agromart-frontend-4p9e1tohj-tasohan6118s-projects.vercel.app/','https://agromart-frontend-lime.vercel.app/','https://agromart-frontend-git-main-tasohan6118s-projects.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'https://agromart-frontend-4p9e1tohj-tasohan6118s-projects.vercel.app','https://agromart-frontend-lime.vercel.app','https://agromart-frontend-git-main-tasohan6118s-projects.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
